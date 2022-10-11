6e25f5160cbd2022ffebba82da8d24cb090573e5
rizomus <104506812+rizomus@users.noreply.github.com>
2022-10-11 20:23:22 +0400

    import modules

--- nikolaev.py
+++ nikolaev.py
@@ -5,6 +5,11 @@ def predict(x):
     Нулевая колонка не используется
     Далее 8 параметров (начиная с индекса 1) в порядке: GGKP, GK, PE, DS, DTR, Wi, BK, BMK
     '''
+    import pandas as pd
+    import numpy as np
+    import pickle
+    from tensorflow.keras.models import load_model
+    
     df_test = pd.read_csv(x, decimal=',' )
     x_test = df_test.iloc[:,[1,2,3,4,5,6,7,8]].values.astype(np.float32)
     model = load_model('/content/model_dense_slicing.keras')
